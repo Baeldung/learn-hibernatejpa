0b8901bf10063e29409238922460c43fafc8e953
lor6 <LORE.CRUSOVEANU@GMAIL.COM>
2025-06-05 13:06:37 +0000

    Merge pull request #48 from Baeldung/module1_JAVA-45650
    [JAVA-45650] Code improvements (module1)

---
+++ setting-up-hibernate-and-jpa-start/src/main/java/com/baeldung/lhj/LhjApp.java
@@ -0,0 +1,9 @@
+package com.baeldung.lhj;
+
+public class LhjApp {
+
+    public static void main(final String... args) {
+
+    }
+
+}
---
+++ the-project-overview/src/test/java/com/baeldung/lhj/ApplicationIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.baeldung.lhj;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest {
+
+    @Test
+    void mainAppMethodIntegrationTest() {
+        LhjApp.main();
+    }
+}
